d8bbfd9108cba797670b26d149881ccdeb3676e9
ThiagoTJB <125403904+ThiagoTJB@users.noreply.github.com>
2023-03-09 17:06:29 -0300

    Add files via upload

---
+++ ProjetoIntegrador/ProjetoIntegrador/frmContrato.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace ProjetoIntegrador
+{
+    partial class frmContrato
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmContrato
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 489);
+            this.Name = "frmContrato";
+            this.Text = "frmContrato";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
